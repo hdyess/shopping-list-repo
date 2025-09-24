150991a957ed042cc31ec923afe19c8fb2a98720
hdyess <henrypdyess@gmail.com>
2025-09-24 11:48:33 -0400

    inital files

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,9 +1,23 @@
 package com.pluralsight;
 
 public class ShoppingList{
+
     public static void main(String[] args){
 
-        System.out.printf("gamer");
+        System.out.println("Hello and welcome!");
 
+        for (int i = 1; i <= 5; i++){
+            System.out.println("i= " + i);
+        }
+
+        doSomething();
     }
+
+    public static void doSomething(){
+        System.out.println("Doing something...");
+        Toolbox.saw();
+    }
+
+
+
 }
---
+++ src/main/java/com/pluralsight/Toolbox.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class Toolbox {
+    public static void saw(){
+        System.out.print("buzz");
+    }
+}
